f23f2bd4ac00191da26d08e9e62135cfdef2825b
Mert Gör <mertgor@masscollabs.xyz>
2023-05-20 13:38:27 +0300

    more while examples

---
+++ walrus.alternative.2.py
@@ -0,0 +1,4 @@
+s =  input('Bir yazı giriniz:')
+while s != 'quit':
+    print(s[::-1])
+    s = input('Bir yazı giriniz:')
---
+++ walrus.alternative.py
@@ -0,0 +1,5 @@
+while True:
+    s = input('Bir yazı giriniz:')
+    if s == 'quit':
+        break
+    print(s[::-1])
