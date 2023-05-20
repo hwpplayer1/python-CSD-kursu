faabeabea5969ef0ab6b60f5ccecefff6b30058c
Mert Gör <mertgor@masscollabs.xyz>
2023-05-20 07:17:59 +0300

    more if examples

---
+++ true-false-else-if.py
@@ -0,0 +1,16 @@
+a = int(input("Bir değer giriniz:"))
+if a:
+    print('Doğru')
+else:
+    print('Yanlış')
+b = []
+if b:
+    print('Doğru')
+else:
+    print('Yanlış')
+
+s = input('Bir yazı giriniz:')
+if s:
+    print('Doğru')
+else:
+    print('Yanlış')
