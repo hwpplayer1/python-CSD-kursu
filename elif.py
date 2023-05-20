2ff57daf392d0ee61ba727273ad7283f6aef5b1a
Mert Gör <mertgor@masscollabs.xyz>
2023-05-20 06:45:05 +0300

    more if examples

---
+++ elif.py
@@ -0,0 +1,7 @@
+a = int(input("Değer giriniz:"))
+if a > 0:
+    print("Pozitif")
+elif a < 0:
+    print("Negatif")
+else:
+    print("Sıfır")
---
+++ else-if-2.py
@@ -0,0 +1,8 @@
+a = int(input("Değer giriniz:"))
+if a > 0:
+    print("Pozitif")
+else:
+    if a < 0:
+        print("Negatif")
+    else:
+        print("Sıfır")
