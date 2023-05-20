0b8a159a9ba9a02186b4c11ed9699276e9a38071
Mert Gör <mertgor@masscollabs.xyz>
2023-05-20 06:57:16 +0300

    else-if-3.py

---
+++ else-if-3.py
@@ -0,0 +1,14 @@
+a = int(input("Bir sayı giriniz:"))
+if a == 1:
+    print("Bir")
+else:
+    if a == 2:
+        print("İki")
+    else:
+        if a == 3:
+            print("Üç")
+        else:
+            if a == 4:
+                print("Dört")
+            else:
+                print("Diğer")
