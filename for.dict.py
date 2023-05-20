15ed46213a2af2bd2cd4a5ba3440b6a9f195a0a6
Mert Gör <mertgor@masscollabs.xyz>
2023-05-20 14:49:34 +0300

    more fore examples

---
+++ for.dict.py
@@ -0,0 +1,4 @@
+d = {'ali': 123, 'veli': 765, 'selami': 745, 'ayşe': 271, 'fatma': 754}
+
+for key, value in d.items():
+    print(key, value)
---
+++ for.tuple.2.py
@@ -0,0 +1,8 @@
+a = [('ali', 10), ('veli', 20), ('selami', 30), ('ayşe', 40), ('fatma', 50)]
+b = []
+
+for name, no in a:
+    b.append((no, name))
+
+print(a)
+print(b)
