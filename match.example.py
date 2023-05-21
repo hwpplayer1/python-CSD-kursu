ba6c5f32c04c5f1ab82ab2178edaa2e9311dcd43
Mert Gör <mertgor@masscollabs.xyz>
2023-05-21 11:17:57 +0300

    match example

---
+++ match.example.py
@@ -0,0 +1,15 @@
+a = int(input('bir değer giriniz:'))
+
+match a:
+    case 1:
+        print('bir')
+    case 2:
+        print('iki')
+    case 3:
+        print('üç')
+    case 4:
+        print('dört')
+    case 5:
+        print('beş')
+    case _:
+        print('hiçbiri')
