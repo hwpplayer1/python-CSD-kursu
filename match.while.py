f0c6948b238774134c0f34308aef510c8380721f
Mert Gör <mertgor@masscollabs.xyz>
2023-05-21 12:21:33 +0300

    fixed match while example

--- match.while.py
+++ match.while.py
@@ -1,13 +1,13 @@
 while True:
     cmd = input('CSD>').strip()
-    match  cmd:
-         case 'delete' | 'remove'   'erase':
-             print('silme işlemi')
-         case 'copy':
-             print('kopyalama işlemi')
-         case 'rename':
-             print('isim değiştirme işlemi')
-         case 'exit'  |  'quit':
-             break
-         case _:
-             print('geçersiz komut!')
+    match cmd:
+        case 'delete' | 'remove' 'erase':
+            print('silme işlemi')
+        case 'copy':
+            print('kopyalama işlemi')
+        case 'rename':
+            print('isim değiştirme işlemi')
+        case 'exit' | 'quit':
+            break
+        case _:
+            print('geçersiz komut')
