eafc72e6187a34d3e03f336800269de01aead73f
Mert Gör <mertgor@masscollabs.xyz>
2023-05-21 11:01:27 +0300

    match examples

---
+++ match.if.py
@@ -0,0 +1,14 @@
+val = int(input('Bir değer giriniz:'))
+
+if val ==  1:
+    print('bir')
+elif val == 2:
+    print('iki')
+elif val == 3:
+    print('üç')
+elif val == 4:
+    print('dört')
+elif val == 5:
+    print('beş')
+else:
+    print('hiçbiri')
