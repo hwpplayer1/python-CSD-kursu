4c320ab787c914cfaa7bd1d08412b333b4e85de3
Mert Gör <mertgor@masscollabs.xyz>
2023-05-21 17:30:36 +0300

    koşul operatörü örnekleri eklendi

---
+++ kosul.operatoru.2.py
@@ -0,0 +1,5 @@
+a = int(input('a:'))
+b = int(input('b:'))
+
+max = a if a > b else b
+print(max)
---
+++ kosul.operatoru.3.py
@@ -0,0 +1,5 @@
+a = int(input('a:'))
+b = int(input('b:'))
+
+c = (a if a > b else b) + 100
+print(c)
