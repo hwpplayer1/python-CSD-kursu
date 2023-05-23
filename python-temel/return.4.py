fa0b7c73b8269ad9e6b5220747b6906c654423a3
Mert Gör <mertgor@masscollabs.xyz>
2023-05-23 10:06:56 +0300

    return example 4

---
+++ python-temel/return.4.py
@@ -0,0 +1,11 @@
+def gettotal(l):
+    total = 0
+    for i in l:
+        total += i
+    return total
+
+result = gettotal([1, 2, 3, 4, 5])
+print(result)
+
+result = gettotal(range(10))
+print(result)
