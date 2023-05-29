b9d87fe21b4644438c9583a1bea99699bf896fea
Mert Gör <mertgor@masscollabs.xyz>
2023-05-29 15:22:03 +0300

    randint.py

---
+++ python-temel/randint.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    val = random.randint(0, 99)
+    print(val, end = ' ')
+# Rassal sayaç rastgele bir tohum değerden başlamaktadır. Dolayısıyla her çalıştırmada rastgele bir dizilim elde edilir.
