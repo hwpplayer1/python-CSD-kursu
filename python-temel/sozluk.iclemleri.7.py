824faa026d95ce311a9b36c880d700c6ecab4397
Mert Gör <mertgor@masscollabs.xyz>
2023-06-12 14:38:00 +0300

    Sözlük İçlemleri örnekleri

---
+++ python-temel/sozluk.iclemleri.7.py
@@ -0,0 +1,3 @@
+d1 = {'ali': 123, 'veli': 432, 'selami': 892, 'ayşe': 45, 'fatma': 574}
+d2 = {d1[key]: key for key in d1}
+print(d2)
---
+++ python-temel/sozluk.iclemleri.8.py
@@ -0,0 +1,3 @@
+d1 = {'ali': 123, 'veli': 432, 'selami': 892, 'ayşe': 45, 'fatma': 574}
+d2 = {value: key for key, value in d1.items()}
+print(d2)
