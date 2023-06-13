2ae3293bfe0e0bf6b6e6adaaedc0a25c45d39297
Mert Gör <mertgor@masscollabs.xyz>
2023-06-13 14:31:58 +0300

    zip unzip examples

---
+++ python-temel/zip.unzip.2.py
@@ -0,0 +1,5 @@
+a = [(10, 'ali'), (20, 'veli'), (30, 'selami')]
+
+result = zip(*a)
+for t in result:
+    print(t)
---
+++ python-temel/zip.unzip.3.py
@@ -0,0 +1,4 @@
+a = [(10, 'ali'), (20, 'veli'), (30, 'selami')]
+
+x, y = zip(*a)
+print(x, y)
