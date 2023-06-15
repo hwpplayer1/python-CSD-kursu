29fa6db8975d0856f6ea020ee4aa62d715a10756
Mert Gör <mertgor@masscollabs.xyz>
2023-06-15 17:55:48 +0300

    pt öznitelik

---
+++ python-temel/pt.oznitelik.py
@@ -0,0 +1,8 @@
+class Point:
+    pass
+
+pt = Point()
+pt.x = 10                    # pt nesnesinin x isimli bir özniteliğini oluşturduk
+pt.y = 20                    # pt nesnesinin y isimli bir özniteleğini oluşturduk
+
+print("x = {}, y = {}".format(pt.x, pt.y))
