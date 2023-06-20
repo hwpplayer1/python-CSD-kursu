538138eef4855361606cba56e85c4f98a2634a16
Mert Gör <mertgor@masscollabs.xyz>
2023-06-20 23:35:12 +0300

    datetime compare

---
+++ python-temel/datetime.compare.py
@@ -0,0 +1,11 @@
+import datetime
+
+d1 = datetime.date(1999, 8, 17)
+d2 = datetime.date(1999, 8, 18)
+
+if d1 > d2:
+    print('d1 > d2')
+elif d1 < d2:
+    print('d1 > d2')
+else:
+    print('d1 == d2')
