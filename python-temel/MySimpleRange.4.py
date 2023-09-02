3ae332c74ba803c250f676f894ebf3990ae372cc
Mert Gör <mertgor@masscollabs.xyz>
2023-09-02 13:07:00 +0300

    My Simple Range example 4

---
+++ python-temel/MySimpleRange.4.py
@@ -0,0 +1,28 @@
+class MySimpleRange:
+    def __init__(self, stop):
+        self.stop = stop
+
+    def __iter__(self):
+        return MySimpleRangeIterator(self.stop)
+
+class MySimpleRangeIterator:
+    def __init__(self, stop):
+        self.stop = stop
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.count += 1
+        if self.count > self.stop:
+            raise StopIteration
+        return self.count - 1
+
+r = MySimpleRange(10)
+iterator = r.__iter__()
+for i in iterator:
+    print(i, end=' ')
+print()
+for i in iterator:
+    print(i, end=' ')
