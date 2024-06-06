07cedf0ea3b16e39b07bec59af113ba1b3e98bb2
Mert Gör <mertgor@masscollabs.xyz>
2024-06-06 04:23:02 +0300

    Python App examples initialized

---
+++ python-app/calendar_code.py
@@ -0,0 +1,4 @@
+import calendar
+
+cal = calendar.TextCalendar()
+cal.prmonth(2024, 6)
---
+++ python-app/time.py
@@ -0,0 +1,10 @@
+import time
+
+start = time.time()
+
+for i in range(100000000):
+    pass
+
+end = time.time()
+
+print(end-start)
