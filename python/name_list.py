d03f58609c2121ae57f3bafbbf13758c8a93ce98
Mert Gör <mertgor@masscollabs.xyz>
2024-06-09 07:23:32 +0300

    Name List example with for loop

---
+++ python/name_list.py
@@ -0,0 +1,14 @@
+"""
+>>> names = ["ali", "veli", "selami"]
+>>> for name in names:
+...     if name == "ali":
+...         print("ali")
+... 
+ali
+"""
+
+names = ["Ali", "Veli", "Selami"]
+
+for name in names:
+    if name == "Ali":
+        print("Ali is here!")
