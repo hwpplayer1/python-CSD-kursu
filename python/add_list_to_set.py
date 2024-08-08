2ee8030f00184a3839ad0b65f3ecc6229d3f7fbd
Mert Gör <mertgor@masscollabs.xyz>
2024-08-08 15:36:40 +0300

    deleted python code

--- python/add_list_to_set.py
+++
@@ -1,9 +0,0 @@
-"""
-Experimental code written by Mert Gör(hwpplayer1)
-GPLv3-or-later 
-"""
-
-mert = set('mert')
-gor = ['g','o','r']
-mert.update(gor)
-print(mert)
--- python/hello_world.py
+++
@@ -1,9 +0,0 @@
-# First code for Python Programming Language
-# Let's write a traditional "Hello World" with print
-
-print("Hello World from Ubuntu 22.04 LTS/Ubuntu Pro with Python Programming Language")
-
-"""
-Let's send our code to our git repository !
-"""
-
