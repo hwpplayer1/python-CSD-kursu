449ae53fe9c89fbe6b9b3956a86881020d782818
Mert Gör <mertgor@masscollabs.xyz>
2025-04-03 19:11:35 +0300

    OS version script

--- python/main.py
+++ python/main.py
@@ -1,1 +1,16 @@
-print("hello python !")
+import platform
+import distro
+
+def detect_os():
+    os_name = platform.system()
+    if os_name == "Linux":
+        linux_dist = distro.name()
+        linux_version = distro.version()
+        print(f"Operating System: GNU/Linux")
+        print(f"Distribution: {linux_dist}")
+        print(f"Version: {linux_version}")
+    else:
+        print(f"Operating System: {os_name}")
+
+if __name__ == "__main__":
+    detect_os()
