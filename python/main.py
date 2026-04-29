e19c9add8046a55fd91e89ee9cc4c1e15b1714da
Mert Gör <mertgor@qbnetworks.xyz>
2026-04-29 05:57:32 +0300

    loop
    Signed-off-by: Mert Gör <mertgor@qbnetworks.xyz>

--- python/main.py
+++ python/main.py
@@ -1,2 +1,14 @@
-a = input("bir metin giriniz ")
-print(a)
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+
+if a > b:
+    if a > c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b > c:
+        print(b)
+    else:
+        print(c)
